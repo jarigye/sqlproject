e1fc6eb55a3b83b5a7bdf48087921e9f8208a06f
jarigye <jorynearigye@gmail.com>
2017-11-18 18:23:14 +0200

    UI

--- app/src/main/java/com/example/jarigye/sqlliteapplication/DbHelper.java
+++ app/src/main/java/com/example/jarigye/sqlliteapplication/DbHelper.java
@@ -9,13 +9,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by jarigye on 11/17/2017.
  */
 class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Students.db";
+    public static final String DATABASE_NAME = "SQLITASSIGN.db";
     public static final String COURSE_TABLE_NAME = "courses";
     public static final String COURSE_COLUMN_ID = "id";
     public static final String COURSE_COLUMN_NAME = "name";
@@ -30,8 +31,8 @@ class DBHelper extends SQLiteOpenHelper {
     public static final String MARKS_COLUMN_ID = "id";
     public static final String MARKS_COLUMN_NAME = "mark";
     public static final String COURSE_MARKS = "coursemarks";
-    public static final String STUDENT_MARKS = "studentmarks";
-    public static final String QUIZ_MARKS = "quizmarks";
+    public static final String QUIZ_COURSE = "quizcourse";
+    public static final String QUIZ_STUDENT = "quizstudent";
 
 
     private HashMap hp;
@@ -40,28 +41,21 @@ class DBHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME , null, 1);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        // TODO Auto-generated method stub
+    @Override public void onCreate(SQLiteDatabase db) {
+//        // TODO Auto-generated method stub
         db.execSQL(
-                "create table " + COURSE_TABLE_NAME + " ( " + COURSE_COLUMN_ID + " INTEGER PRIMARY KEY," + COURSE_COLUMN_NAME + "VARCHAR);");
+                "create table " + COURSE_TABLE_NAME + " ( " + COURSE_COLUMN_ID + "   INTEGER PRIMARY KEY," + COURSE_COLUMN_NAME + "   VARCHAR);");
         db.execSQL(
-                "create table " + STUDENTS_TABLE_NAME + " ( " + STUDENTS_COLUMN_ID + " INTEGER PRIMARY KEY ," + STUDENTS_UNIQUE_NO + "INTEGER);");
+                "create table " + STUDENTS_TABLE_NAME + " ( " + STUDENTS_COLUMN_ID + "  INTEGER PRIMARY KEY ," + STUDENTS_UNIQUE_NO + "  INTEGER);");
         db.execSQL(
-                "create table " + QUIZ_TABLE_NAME + " ( " + QUIZ_COLUMN_ID + " INTEGER PRIMARY KEY ," + QUIZ_COLUMN_NAME + "VARCHAR);");
-        db.execSQL(
-                "create table "
-                        + MARKS_TABLE_NAME + "(" + MARKS_COLUMN_ID + " INTEGER PRIMARY KEY ,"
-                        + MARKS_COLUMN_NAME + " FLOAT,"
-                        + COURSE_MARKS + " INTEGER,"
-                        + STUDENT_MARKS + "  INTEGER,"
-                        + QUIZ_MARKS + " INTEGER,"
-                        +" FOREIGN KEY (" + COURSE_MARKS + ") REFERENCES " + COURSE_TABLE_NAME + "(" + COURSE_COLUMN_ID + ")" +","+
-                        " FOREIGN KEY (" + STUDENT_MARKS + ") REFERENCES " + STUDENTS_TABLE_NAME + "(" + STUDENTS_COLUMN_ID + ")" +","+
-                        " FOREIGN KEY (" + QUIZ_MARKS + ") REFERENCES " + QUIZ_TABLE_NAME + "(" + QUIZ_COLUMN_ID + "));");
+                "create table " + QUIZ_TABLE_NAME + " ( " + QUIZ_COLUMN_ID + "   INTEGER PRIMARY KEY ," + QUIZ_COLUMN_NAME + "   VARCHAR,"
+                        + QUIZ_STUDENT + "   INTEGER,"
+                        + QUIZ_COURSE + "    INTEGER,"
+                        + MARKS_COLUMN_NAME + "   FLOAT,"
+                        + " FOREIGN KEY (" + QUIZ_STUDENT + ") REFERENCES " + STUDENTS_TABLE_NAME + "(" + STUDENTS_COLUMN_ID + ")" +","+
+                       " FOREIGN KEY (" + QUIZ_COURSE + ") REFERENCES " + COURSE_TABLE_NAME + "(" + COURSE_COLUMN_ID + "));");
 
-
-    }
+   }
 
 
     @Override
@@ -69,7 +63,6 @@ class DBHelper extends SQLiteOpenHelper {
         super.onOpen(db);
         if (!db.isReadOnly()) {
             // Enable foreign key constraints
-           // db.execSQL("PRAGMA foreign_keys= on;");
             db.setForeignKeyConstraintsEnabled (true);
         }
     }
@@ -77,75 +70,58 @@ class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // TODO Auto-generated method stub
         db.execSQL("DROP TABLE IF EXISTS courses");
-        db.execSQL("DROP TABLE IF EXISTS students");
-        db.execSQL("DROP TABLE IF EXISTS quiz");
-        db.execSQL("DROP TABLE IF EXISTS marks");
+       db.execSQL("DROP TABLE IF EXISTS students");
+       db.execSQL("DROP TABLE IF EXISTS quiz");
         onCreate(db);
     }
 
     public void insertCourse() {
         database = this.getReadableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COURSE_COLUMN_NAME, "JAVA");
-        contentValues.put(COURSE_COLUMN_NAME, "DATA");
-        database.insert(COURSE_TABLE_NAME, null, contentValues);
+        database.beginTransaction();
+        ArrayList<String> x= new ArrayList();
+        x.add("Programming 101");
+        x.add("Programming 102");
+        for(String text : x) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COURSE_COLUMN_NAME, text);
+            database.insert(COURSE_TABLE_NAME, null, contentValues);
+        }
+        database.setTransactionSuccessful();
+        database.endTransaction();
         database.close();
     }
-    public void insertQuiz() {
-        database = this.getReadableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COURSE_COLUMN_NAME, "Q1");
-        contentValues.put(COURSE_COLUMN_NAME, "Q2");
-        contentValues.put(COURSE_COLUMN_NAME, "Q3");
-        contentValues.put(COURSE_COLUMN_NAME, "Q4");
-        contentValues.put(COURSE_COLUMN_NAME, "Q5");
-        database.insert(QUIZ_TABLE_NAME, null, contentValues);
-        database.close();
-    }
+
     public void insertStudent() {
         database = this.getReadableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(STUDENTS_UNIQUE_NO, "1234");
-        contentValues.put(STUDENTS_UNIQUE_NO, "1032");
-        contentValues.put(STUDENTS_UNIQUE_NO, "1782");
-        contentValues.put(STUDENTS_UNIQUE_NO, "2314");
-        database.insert(STUDENTS_TABLE_NAME, null, contentValues);
+        database.beginTransaction();
+       ArrayList<String> arrList = new ArrayList<>();
+        arrList.add("1234");
+        arrList.add("1032");
+        arrList.add("5466");
+        arrList.add("5622");
+        arrList.add("1002");
+        for(String text : arrList)
+        {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(STUDENTS_UNIQUE_NO, text);
+            database.insert(STUDENTS_TABLE_NAME, null, contentValues);
+        }
+        database.setTransactionSuccessful();
+        database.endTransaction();
         database.close();
     }
-//    public void insertMark(MarksModel mark) {
-//        database = this.getReadableDatabase();
-//        ContentValues contentValues = new ContentValues();
-//        contentValues.put(MARKS_COLUMN_NAME, mark.getMark());
-//        contentValues.put(MARKS_COLUMN_NAME, mark.getQuiz());
-//        contentValues.put(MARKS_COLUMN_NAME, mark.getCourse());
-//        contentValues.put(MARKS_COLUMN_NAME, mark.getStudent());
-//        database.insert(MARKS_TABLE_NAME, null, contentValues);
-//        database.close();
-//    }
-    public void insertMark() {
+
+    public void insertQuiz() {
         database = this.getReadableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MARKS_COLUMN_NAME, "52");
-        contentValues.put(COURSE_MARKS, "1");
-        contentValues.put(STUDENT_MARKS, "1");
-        contentValues.put(QUIZ_MARKS, "1");
-        contentValues.put(MARKS_COLUMN_NAME, "56");
-        contentValues.put(COURSE_MARKS, "1");
-        contentValues.put(STUDENT_MARKS, "1");
-        contentValues.put(QUIZ_MARKS, "2");
-        contentValues.put(MARKS_COLUMN_NAME, "66");
-        contentValues.put(COURSE_MARKS, "1");
-        contentValues.put(STUDENT_MARKS, "1");
-        contentValues.put(QUIZ_MARKS, "3");
-        contentValues.put(MARKS_COLUMN_NAME, "89");
-        contentValues.put(COURSE_MARKS, "1");
-        contentValues.put(STUDENT_MARKS, "1");
-        contentValues.put(QUIZ_MARKS, "4");
-        contentValues.put(MARKS_COLUMN_NAME, "70");
-        contentValues.put(COURSE_MARKS, "1");
-        contentValues.put(STUDENT_MARKS, "1");
-        contentValues.put(QUIZ_MARKS, "5");
-        database.insert(MARKS_TABLE_NAME, null, contentValues);
+        database.beginTransaction();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(QUIZ_COLUMN_NAME, "Q1");
+            contentValues.put(QUIZ_STUDENT, "1");
+            contentValues.put(QUIZ_COURSE, "1");
+            contentValues.put(MARKS_COLUMN_NAME, "52");
+        database.insert(QUIZ_TABLE_NAME, null, contentValues);
+        database.setTransactionSuccessful();
+        database.endTransaction();
         database.close();
     }
 
@@ -166,26 +142,77 @@ class DBHelper extends SQLiteOpenHelper {
 
     public Cursor getData(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from marks where id="+id+"", null );
+        //Cursor res1 =  db.rawQuery( "select * from marks where id="+id+"", null );
+        Cursor res =  db.rawQuery( "select * from courses", null );
         return res;
     }
 
-    public int numberOfRows(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        int numRows = (int) DatabaseUtils.queryNumEntries(db, MARKS_TABLE_NAME);
-        return numRows;
-    }
 
-    public ArrayList<String> getAllStudents() {
+    public ArrayList<String> getRows(){
+        String query =  " SELECT  id,name, quizstudent ,(select unique_no from students where id=quizstudent) as unique_no, mark FROM quiz where quizcourse =1";
+        SQLiteDatabase db = getWritableDatabase();
+
+        Cursor  cursor = db.rawQuery(query,null);
+        ArrayList<String> list= new ArrayList<String>();
+
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                String name = cursor.getString(cursor.getColumnIndex("id"));
+                list.add(name);
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        db.close();
+        return list;
+    }
+    public List<String> getAllCourses(){
+        List<String> courses = new ArrayList<String>();
+
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + COURSE_TABLE_NAME;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                courses.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        // closing connection
+        cursor.close();
+        db.close();
+        // returning lables
+        return courses;
+    }
+    public int getId(String course){
+        int id = 0;
+        String h =course;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res1 =  db.rawQuery( "select id from courses where name='"+h+"'", null );
+        res1.moveToFirst();
+        id=res1.getInt(0);
+        return id;
+    }
+    public ArrayList<String> getAllStudents(String course) {
+        
         ArrayList<String> array_list = new ArrayList<String>();
 
         //hp = new HashMap();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from students", null );
+        String selectQuery = "SELECT  id  FROM courses where name like " + course;
+
+        Cursor res1 =  db.rawQuery(selectQuery, null );
+        
+        Cursor res =  db.rawQuery( "SELECT  (select unique_no from students where id=quizstudent)as unique_no ,*FROM quiz where quizstudent=1", null );
         res.moveToFirst();
 
         while(res.isAfterLast() == false){
-            array_list.add(res.getString(res.getColumnIndex(STUDENTS_UNIQUE_NO)));
+            array_list.add(res.getString(res.getColumnIndex("unique_no")));
+            array_list.add(res.getString(res.getColumnIndex("name")));
+            array_list.add(res.getString(res.getColumnIndex("mark")));
             res.moveToNext();
         }
         return array_list;
--- app/src/main/java/com/example/jarigye/sqlliteapplication/MainActivity.java
+++ app/src/main/java/com/example/jarigye/sqlliteapplication/MainActivity.java
@@ -3,16 +3,101 @@ package com.example.jarigye.sqlliteapplication;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  implements
+        AdapterView.OnItemSelectedListener {
     DBHelper mydb;
+    private Spinner spinner;
+    private TableLayout tableLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mydb = new DBHelper(this);
-        mydb.insertCourse();
-        mydb.insertStudent();
-        mydb.insertQuiz();
+
+       // mydb.insertCourse();
+       // mydb.insertStudent();
+       // mydb.insertQuiz();
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        loadSpinnerData();
+
+        tableLayout=(TableLayout)findViewById(R.id.tableLayout);
+
+        //for (int i=0;i<5;i++){
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+        TextView quiz_display_s1  = new TextView(this);
+        quiz_display_s1.setText("");
+        quiz_display_s1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        TextView quiz_display_q1  = new TextView(this);
+        quiz_display_q1.setText("QUIZ 1");
+        quiz_display_q1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tr.addView(quiz_display_q1);
+        TextView quiz_display_q2  = new TextView(this);
+        quiz_display_q2.setText("QUIZ 2");
+        quiz_display_q2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tr.addView(quiz_display_q2);
+        TextView quiz_display_q3  = new TextView(this);
+        quiz_display_q3.setText("QUIZ 3");
+        quiz_display_q3.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tr.addView(quiz_display_q3);
+        TextView quiz_display_q4  = new TextView(this);
+        quiz_display_q4.setText("QUIZ 4");
+        tr.addView(quiz_display_q4);
+        quiz_display_q4.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        TextView quiz_display_q5  = new TextView(this);
+        quiz_display_q5.setText("QUIZ 5");
+        quiz_display_q5.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tr.addView(quiz_display_q5);
+        tableLayout.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
     }
+    private void loadSpinnerData() {
+        // database handler
+           DBHelper db = new DBHelper(getApplicationContext());
+        // Spinner Drop down elements
+        List<String> courses = db.getAllCourses();
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, courses);
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+         DBHelper db = new DBHelper(getApplicationContext());
+        // On selecting a spinner item
+        String course = parent.getItemAtPosition(position).toString();
+      int a=  db.getId(course);
+       // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "You selected: " + a,
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
